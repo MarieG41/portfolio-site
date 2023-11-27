d13536c6acc8db9f96bbb55c476b460783947e3e
MarieG41 <mariegond8@gmail.com>
2023-11-27 15:26:05 +0100

    clean code

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const canvas = document.querySelector("#canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const orangeCircle = document.createElement("img");
+const lightGreenCircle = document.createElement("img");
+
+orangeCircle.src = "images/Ellipse 6.png";
+lightGreenCircle.src = "images/Ellipse 7.png";
+
+let context = canvas.getContext("2d");
+
+const mouseMoveHandle = (event) => {
+    const left = event.clientX;
+    const top = event.clientY;
+    
+    context.drawImage(orangeCircle, left, top);
+    context.drawImage(lightGreenCircle, left, top);
+}
+
+canvas.addEventListener("mousemove", mouseMoveHandle);
+
+
 let buttonTop = document.getElementById("btn-top");
 
 window.onscroll =function() {scrollFunction()};
